50b5ec6aaf349034d11a24ce34c37b19b443e76d
ilightman <maximlightman@gmail.com>
2021-07-11 16:14:32 +0300

    v.1.01
    small refactoring

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ import logging
 from func import delivery, url_short
 from aiogram import Bot, Dispatcher, executor, types, filters
 
+from config import API_TOKEN
 
-API_TOKEN = '1702734379:AAFyR86LYr4mMXEnRT12yvRdvybSD1-gSy4'
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 # Initialize bot and dispatcher
@@ -13,18 +13,18 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(filters.Regexp(r'^\d{6}'))
 async def mail_delivery_period(message: types.Message):
-    '''
+    """
     При отправке 6 символов возвращает ответ Почты России по срокам доставки
-    '''
+    """
     await message.answer(delivery(message.text))
 
 
 @dp.message_handler(filters.Regexp(r'^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*'))
 async def url_shortener(message: types.Message):
-    '''
+    """
     При отправке ссылки возвращает сокращенную
-    '''
-    await message.answer(url_short(message.text), disable_web_page_preview = True)
+    """
+    await message.answer(url_short(message.text), disable_web_page_preview=True)
 
 
 @dp.message_handler(commands=['start', 'help'])
@@ -37,9 +37,9 @@ async def send_welcome(message: types.Message):
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    '''
+    """
     echo
-    '''
+    """
     await message.answer(message.text)
 
 
