ea6b92d2f690be5df8b8281d5f6c4dd0629a5b20
ilightman <maximlightman@gmail.com>
2021-08-20 20:24:06 +0300

    -add functions
    -add url mini description func
    -add another function to post delivery days

--- main.py
+++ main.py
@@ -1,49 +1,72 @@
 import logging
 import os
-from src.func import delivery, url_short
+from src.func import delivery, url_short, mini_description
 from aiogram import Bot, Dispatcher, executor, types, filters
 
-
-# Configure logging
 logging.basicConfig(level=logging.INFO)
-# Initialize bot and dispatcher
-bot = Bot(token=os.environ['ASTUDIO_BOT_TOKEN'])
+bot = Bot(token=os.getenv("ASTUDIO_BOT_TOKEN"), parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
 
 
+@dp.callback_query_handler(filters.Text(startswith='url'))
+async def process_callback_button_url(callback_query: types.CallbackQuery):
+    code = callback_query.data
+    url = callback_query.message.text
+    await callback_query.message.delete()
+    if code == 'url1':
+        await bot.send_message(callback_query.message.chat.id, url_short(url), disable_web_page_preview=True)
+    elif code == 'url2':
+        await bot.send_message(callback_query.message.chat.id, mini_description(url))
+
+
 @dp.message_handler(filters.Regexp(r'^\d{6}$'))
-async def delivery_counter1(message: types.Message):
+async def delivery_only_index(message: types.Message):
     """
-    If User message is 6 digit return delivery
+        If User message is 6 digit return delivery days
     """
-    delivery_response = delivery(p_index=message.text)
+    delivery_response = delivery(to_index=message.text)
     await message.answer(delivery_response)
 
 
-@dp.message_handler(filters.Regexp(r'^\d{6}\s\d{4,}\s\d{4}$'))
-async def delivery_counter2(message: types.Message):
+@dp.message_handler(filters.Regexp(r'^\d{6}(\s|\S)\d{3,}(\s|\S)\d{4,}$'))
+async def delivery_index_price_weight(message: types.Message):
     """
-        If User message is 6 digit + 4-5 digit + 4 digit return delivery and price
+        If User message is 6 digit index, 3+ digit weight, 4+ digit price return delivery days and price
     """
-    p_index, weight, price = message.text.split()
-    delivery_response = delivery(p_index=p_index, weight=weight, price=price)
+    to_index, weight, price = message.text.split(message.text[6])
+    delivery_response = delivery(to_index=to_index, weight=weight, price=price)
     await message.answer(delivery_response)
 
 
 @dp.message_handler(filters.Regexp(r'^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*'))
 async def url_shortener(message: types.Message):
     """
-    If User message is URL, returns shortened_url
+        If User message is URL, then user choose url_short or product_description
     """
-    await message.answer(url_short(message.text), disable_web_page_preview = True)
+    kb_inl = types.InlineKeyboardMarkup(row_width=2)
+    kb_inl.add(types.InlineKeyboardButton('Сократить ссылку', callback_data=f'url1'))
+    kb_inl.add(types.InlineKeyboardButton('Краткое описание', callback_data=f'url2'))
+
+    await message.answer(message.text, disable_web_page_preview=True, reply_markup=kb_inl)
 
 
 @dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
+async def print_menu(message):
     """
-    This handler will be called when user sends `/start` or `/help` command
+        This handler will be called when user sends `/start` or `/help` command
     """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+    message_text = 'Вот, что умеет этот бот:\n' \
+                   '/start , /help - отображает список доступных команд\n' \
+                   '\n' \
+                   '🌐 Сокращатель ссылок + мини описание:\n' \
+                   '├ <code>https://carautostudio.ru/catalog/shtatnye_magnitoly/</code>\n' \
+                   '└ пришлет сокращенную ссылку или краткое описание товара\n' \
+                   '\n' \
+                   '📮 Почта России (в срок доставки уже добавлено 2 дня)\n' \
+                   '├ <code>123456</code> - срок доставки по индексу \n' \
+                   '└ <code>индекс вес цена</code> - стоимость доставки Почты и сроки'
+
+    await message.answer(message_text)
 
 
 @dp.message_handler()
--- src/func.py
+++ src/func.py
@@ -1,32 +1,38 @@
 import urllib3
 import json
-from datetime import datetime
-from aiogram import types
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
 
 
-def delivery(p_index: int, price=None, weight=None, f_index=125476) -> str:
-    if price:
-        url_base = 'https://delivery.pochta.ru/v2/calculate/tariff/delivery?json&object=4040'
+def delivery(to_index: int, weight=None, price=None, from_index=125476) -> str:
+    url_base = 'https://delivery.pochta.ru/v2/calculate'
+    if weight and price:
+        # расчет тарифа и контрольные сроки
+        url_base += '/tariff/delivery?json&object=4040'
         pack = 40 if int(weight) > 2000 else 20 if 1000 < int(weight) <= 2000 else 10  # s-10, m-20, l-30, xl-40
-        params = f'&from=125476&to={p_index}&weight={weight}&pack={pack}&sumoc={price}00'
+        params = f'&from=125476&to={to_index}&weight={weight}&pack={pack}&sumoc={price}00'
     else:
+        # только сроки доставки
         weight = 1000
-        url_base = 'https://delivery.pochta.ru/v2/calculate/delivery?json&object=27040'
-        params = f'&from={f_index}&to={p_index}&weight={weight}&pack=20'
+        url_base += '/delivery?json&object=27040'
+        params = f'&from={from_index}&to={to_index}&weight={weight}&pack=20'
 
-    date = f'&date={datetime.now().strftime("%Y%m%d")}'
+    date = f'&date={(datetime.now() + timedelta(days=2)).strftime("%Y%m%d")}'
+
     http = urllib3.PoolManager()
-    resp_json = json.loads(http.urlopen('GET', url_base + params + date).data)
+    resp_http = http.urlopen('GET', url_base + params + date)
+    resp_json = json.loads(resp_http.data)
 
     try:
         delivery_days = resp_json["delivery"]["max"]
         delivery_deadline = datetime.strptime(resp_json["delivery"]["deadline"][0:8], "%Y%m%d").strftime("%d.%m.%Y")
-        delivery_days_s = f'{delivery_days} дн., до {delivery_deadline}'
+        delivery_days_str = f'Срок доставки:  <b>{delivery_days}</b> дн.\nДоставка:  до <b>{delivery_deadline}</b>'
         if price:
-            paynds = resp_json["paynds"]
-            return f'Ценная, {paynds // 100}.{paynds % 100} руб., ' + delivery_days_s
+            pay_nds = resp_json["paynds"]
+            return f'Стоимость:  <b>{float(pay_nds / 100):5.2f}</b> руб. \n' + delivery_days_str
         else:
-            return delivery_days_s
+            return delivery_days_str
+
     except KeyError:
         return resp_json['errors'][0]['msg']
 
@@ -37,3 +43,20 @@ def url_short(url: str) -> str:
     url_sh = http.urlopen('GET', url_long).data
     return url_sh.decode('UTF-8')
 
+
+def mini_description(url: str) -> str:
+    http = urllib3.PoolManager()
+    url_sh = http.urlopen('GET', url).data
+    soup = BeautifulSoup(url_sh.decode('UTF-8'), 'html.parser')
+    try:
+        pict = ''.join(i for i in str(soup.find_all('img')[1]).split() if i.startswith('src='))[5:-1]
+        pict_url = 'https://carautostudio.ru' + pict
+
+        mini_desc = soup.find(attrs={'class': 'item_info_section product-element-preview-text'})
+        mini_desc = (i.replace('\xa0', '') for i in [i.text for i in mini_desc.find_all('li')])
+        mini_desk = '\n'.join(mini_desc)
+
+        return f'<a href="{pict_url}">{url_short(url)}</a>\n{mini_desk}'
+
+    except AttributeError:
+        return 'Описание не найдено'
